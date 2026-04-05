3fd3810643607c685d819c7320f55942593a36f6
hemant7580 <hemanttomar7580@gmail.com>
2026-04-05 23:35:59 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': { target: 'https://pulse-frontend-271v.onrender.com', changeOrigin: true },
-      '/uploads': { target: 'https://pulse-frontend-271v.onrender.com', changeOrigin: true },
-    },
-  },
 });
