4d328245da37ee502dd80bfead3452a6092f77fa
hemant7580 <hemanttomar7580@gmail.com>
2026-04-05 23:02:34 +0530

    Update CORS origin to production URL

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://pulse-frontend-five-sepia.vercel.app",
     credentials: true,
   })
 );
